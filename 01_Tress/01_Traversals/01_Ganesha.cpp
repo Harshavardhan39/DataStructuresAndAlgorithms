9bcc6c97550cde9372f08de4f28f0e32593bd400
Harshavardhan <harsha392001@gmial.com>
2026-04-26 10:08:43 +0530

    Setting up folder structure

---
+++ 01_Tress/01_Traversals/01_Ganesha.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void ganesha() {
+	int n;
+	cin >> n;
+	while (n--) {
+		cout << "Sita Rama" << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void execution() {
+
+#ifndef ONLINE_JUDGE
+	freopen("../../radhaKrishna.txt", "r", stdin);
+	freopen("../../satyaNarayanaSwamy.txt", "w", stdout);
+#endif
+
+}
+
+int main() {
+
+	execution();
+
+	ganesha();
+
+	return 0;
+
+}
